5e30501ad7b5e9c3009188ec26f2c9311f0c13a4
Vitorsulzbach <37155586+Vitorsulzbach@users.noreply.github.com>
2019-06-28 13:03:31 -0300

    Add files via upload

--- getplate.py
+++ getplate.py
@@ -3,7 +3,7 @@ import cv2
 import PossibleChar
 import PossiblePlate
 import math
-
+import pytesseract
 
 MIN_DIAG_SIZE_MULTIPLE_AWAY = 0.3
 MAX_DIAG_SIZE_MULTIPLE_AWAY = 5.0
@@ -23,159 +23,84 @@ PLATE_WIDTH_PADDING_FACTOR = 1.3
 PLATE_HEIGHT_PADDING_FACTOR = 1.5
 MIN_CONTOUR_AREA = 100
 
-listOfPossiblePlates = []
-
 def distanceBetweenChars(firstChar, secondChar):
 	intX = abs(firstChar.intCenterX - secondChar.intCenterX)
 	intY = abs(firstChar.intCenterY - secondChar.intCenterY)
-
 	return math.sqrt((intX ** 2) + (intY ** 2))
 
 def findListOfMatchingChars(possibleChar, listOfChars):
-            # the purpose of this function is, given a possible char and a big list of possible chars,
-            # find all chars in the big list that are a match for the single possible char, and return those matching chars as a list
-	listOfMatchingChars = []                # this will be the return value
-
-	for possibleMatchingChar in listOfChars:                # for each char in big list
-		if possibleMatchingChar == possibleChar:    # if the char we attempting to find matches for is the exact same char as the char in the big list we are currently checking
-                                                    # then we should not include it in the list of matches b/c that would end up double including the current char
-			continue                                # so do not add to list of matches and jump back to top of for loop
-        # end if
-                    # compute stuff to see if chars are a match
+	listOfMatchingChars = []
+	for possibleMatchingChar in listOfChars:
+		if possibleMatchingChar == possibleChar:
+			continue
 		fltDistanceBetweenChars = distanceBetweenChars(possibleChar, possibleMatchingChar)
-
 		fltAngleBetweenChars = angleBetweenChars(possibleChar, possibleMatchingChar)
-
 		fltChangeInArea = float(abs(possibleMatchingChar.intBoundingRectArea - possibleChar.intBoundingRectArea)) / float(possibleChar.intBoundingRectArea)
-
 		fltChangeInWidth = float(abs(possibleMatchingChar.intBoundingRectWidth - possibleChar.intBoundingRectWidth)) / float(possibleChar.intBoundingRectWidth)
 		fltChangeInHeight = float(abs(possibleMatchingChar.intBoundingRectHeight - possibleChar.intBoundingRectHeight)) / float(possibleChar.intBoundingRectHeight)
-
-                # check if chars match
 		if (fltDistanceBetweenChars < (possibleChar.fltDiagonalSize * MAX_DIAG_SIZE_MULTIPLE_AWAY) and
 			fltAngleBetweenChars < MAX_ANGLE_BETWEEN_CHARS and
 			fltChangeInArea < MAX_CHANGE_IN_AREA and
 			fltChangeInWidth < MAX_CHANGE_IN_WIDTH and
 			fltChangeInHeight < MAX_CHANGE_IN_HEIGHT):
-
-			listOfMatchingChars.append(possibleMatchingChar)        # if the chars are a match, add the current char to list of matching chars
-        # end if
-    # end for
-
-	return listOfMatchingChars                  # return result
+			listOfMatchingChars.append(possibleMatchingChar)
+	return listOfMatchingChars
 
 def angleBetweenChars(firstChar, secondChar):
 	fltAdj = float(abs(firstChar.intCenterX - secondChar.intCenterX))
 	fltOpp = float(abs(firstChar.intCenterY - secondChar.intCenterY))
-
-	if fltAdj != 0.0:                           # check to make sure we do not divide by zero if the center X positions are equal, float division by zero will cause a crash in Python
-		fltAngleInRad = math.atan(fltOpp / fltAdj)      # if adjacent is not zero, calculate angle
+	if fltAdj != 0.0:
+		fltAngleInRad = math.atan(fltOpp / fltAdj)
 	else:
-		fltAngleInRad = 1.5708                          # if adjacent is zero, use this as the angle, this is to be consistent with the C++ version of this program
-    # end if
-
-	fltAngleInDeg = fltAngleInRad * (180.0 / math.pi)       # calculate angle in degrees
-
+		fltAngleInRad = 1.5708
+	fltAngleInDeg = fltAngleInRad * (180.0 / math.pi)
 	return fltAngleInDeg
 
 def findListOfListsOfMatchingChars(listOfPossibleChars):
-            # with this function, we start off with all the possible chars in one big list
-            # the purpose of this function is to re-arrange the one big list of chars into a list of lists of matching chars,
-            # note that chars that are not found to be in a group of matches do not need to be considered further
-	listOfListsOfMatchingChars = []                  # this will be the return value
+	listOfListsOfMatchingChars = []
+	for possibleChar in listOfPossibleChars:
+		listOfMatchingChars = findListOfMatchingChars(possibleChar, listOfPossibleChars)
+		listOfMatchingChars.append(possibleChar)
 
-	for possibleChar in listOfPossibleChars:                        # for each possible char in the one big list of chars
-		listOfMatchingChars = findListOfMatchingChars(possibleChar, listOfPossibleChars)        # find all chars in the big list that match the current char
-
-		listOfMatchingChars.append(possibleChar)                # also add the current char to current possible list of matching chars
-
-		if len(listOfMatchingChars) < MIN_NUMBER_OF_MATCHING_CHARS:     # if current possible list of matching chars is not long enough to constitute a possible plate
-			continue                            # jump back to the top of the for loop and try again with next char, note that it's not necessary
-                                                # to save the list in any way since it did not have enough chars to be a possible plate
-        # end if
-
-                                                # if we get here, the current list passed test as a "group" or "cluster" of matching chars
-		listOfListsOfMatchingChars.append(listOfMatchingChars)      # so add to our list of lists of matching chars
-
+		if len(listOfMatchingChars) < MIN_NUMBER_OF_MATCHING_CHARS:
+			continue
+		listOfListsOfMatchingChars.append(listOfMatchingChars)
 		listOfPossibleCharsWithCurrentMatchesRemoved = []
-
-                                                # remove the current list of matching chars from the big list so we don't use those same chars twice,
-                                                # make sure to make a new big list for this since we don't want to change the original big list
 		listOfPossibleCharsWithCurrentMatchesRemoved = list(set(listOfPossibleChars) - set(listOfMatchingChars))
 
-		recursiveListOfListsOfMatchingChars = findListOfListsOfMatchingChars(listOfPossibleCharsWithCurrentMatchesRemoved)      # recursive call
-
-		for recursiveListOfMatchingChars in recursiveListOfListsOfMatchingChars:        # for each list of matching chars found by recursive call
-			listOfListsOfMatchingChars.append(recursiveListOfMatchingChars)             # add to our original list of lists of matching chars
-        # end for
-
-		break       # exit for
-
-    # end for
-
+		recursiveListOfListsOfMatchingChars = findListOfListsOfMatchingChars(listOfPossibleCharsWithCurrentMatchesRemoved)
+		for recursiveListOfMatchingChars in recursiveListOfListsOfMatchingChars:
+			listOfListsOfMatchingChars.append(recursiveListOfMatchingChars)
+		break
 	return listOfListsOfMatchingChars
 
 def extractPlate(imgOriginal, listOfMatchingChars):
-	possiblePlate = PossiblePlate.PossiblePlate()           # this will be the return value
-
-	listOfMatchingChars.sort(key = lambda matchingChar: matchingChar.intCenterX)        # sort chars from left to right based on x position
-
-            # calculate the center point of the plate
+	possiblePlate = PossiblePlate.PossiblePlate()
+	listOfMatchingChars.sort(key = lambda matchingChar: matchingChar.intCenterX)
 	fltPlateCenterX = (listOfMatchingChars[0].intCenterX + listOfMatchingChars[len(listOfMatchingChars) - 1].intCenterX) / 2.0
 	fltPlateCenterY = (listOfMatchingChars[0].intCenterY + listOfMatchingChars[len(listOfMatchingChars) - 1].intCenterY) / 2.0
-
 	ptPlateCenter = fltPlateCenterX, fltPlateCenterY
-
-            # calculate plate width and height
 	intPlateWidth = int((listOfMatchingChars[len(listOfMatchingChars) - 1].intBoundingRectX + listOfMatchingChars[len(listOfMatchingChars) - 1].intBoundingRectWidth - listOfMatchingChars[0].intBoundingRectX) * PLATE_WIDTH_PADDING_FACTOR)
-
 	intTotalOfCharHeights = 0
-
 	for matchingChar in listOfMatchingChars:
 		intTotalOfCharHeights = intTotalOfCharHeights + matchingChar.intBoundingRectHeight
-    # end for
-
 	fltAverageCharHeight = intTotalOfCharHeights / len(listOfMatchingChars)
-
 	intPlateHeight = int(fltAverageCharHeight * PLATE_HEIGHT_PADDING_FACTOR)
-
-            # calculate correction angle of plate region
 	fltOpposite = listOfMatchingChars[len(listOfMatchingChars) - 1].intCenterY - listOfMatchingChars[0].intCenterY
 	fltHypotenuse = distanceBetweenChars(listOfMatchingChars[0], listOfMatchingChars[len(listOfMatchingChars) - 1])
 	fltCorrectionAngleInRad = math.asin(fltOpposite / fltHypotenuse)
 	fltCorrectionAngleInDeg = fltCorrectionAngleInRad * (180.0 / math.pi)
-
-            # pack plate region center point, width and height, and correction angle into rotated rect member variable of plate
 	possiblePlate.rrLocationOfPlateInScene = ( tuple(ptPlateCenter), (intPlateWidth, intPlateHeight), fltCorrectionAngleInDeg )
-
-            # final steps are to perform the actual rotation
-
-            # get the rotation matrix for our calculated correction angle
 	rotationMatrix = cv2.getRotationMatrix2D(tuple(ptPlateCenter), fltCorrectionAngleInDeg, 1.0)
-
-	height, width, numChannels = imgOriginal.shape      # unpack original image width and height
-
-	imgRotated = cv2.warpAffine(imgOriginal, rotationMatrix, (width, height))       # rotate the entire image
-
+	height, width, numChannels = imgOriginal.shape
+	imgRotated = cv2.warpAffine(imgOriginal, rotationMatrix, (width, height))
 	imgCropped = cv2.getRectSubPix(imgRotated, (intPlateWidth, intPlateHeight), tuple(ptPlateCenter))
-
-	possiblePlate.imgPlate = imgCropped         # copy the cropped plate image into the applicable member variable of the possible plate
-
+	possiblePlate.imgPlate = imgCropped
 	return possiblePlate
 
 def getplate(address):
-	kNearest = cv2.ml.KNearest_create()
-	allContoursWithData = []                # declare empty lists,
-	validContoursWithData = []              # we will fill these shortly
-	npaClassifications = np.loadtxt("classifications.txt", np.float32) # read in training classifications
-	npaFlattenedImages = np.loadtxt("flattened_images.txt", np.float32)# read in training images
-	npaClassifications = npaClassifications.reshape((npaClassifications.size, 1))       # reshape numpy array to 1d, necessary to pass to call to train
-	kNearest.setDefaultK(1)                                                             # set default K to 1
-	kNearest.train(npaFlattenedImages, cv2.ml.ROW_SAMPLE, npaClassifications)           # train KNN object
-
+	listOfPossiblePlates = []
 	imgOriginalScene  = cv2.imread(address)
-
-
 	ADAPTIVE_THRESH_WEIGHT = 9
 	ADAPTIVE_THRESH_BLOCK_SIZE = 19
 	GAUSSIAN_SMOOTH_FILTER_SIZE = (5, 5)
@@ -202,56 +127,81 @@ def getplate(address):
 	listOfPossibleChars = []
 	intCountOfPossibleChars = 0
 	imgThreshCopy = imgThresh.copy()
-	contours, npaHierarchy = cv2.findContours(imgThreshCopy, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)   # find all contours
+	contours, npaHierarchy = cv2.findContours(imgThreshCopy, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 	height1, width1 = imgThresh.shape
 	imgContours = np.zeros((height1, width1, 3), np.uint8)
-	for i in range(0, len(contours)):                       # for each contour
+	for i in range(0, len(contours)):
 		possibleChar = PossibleChar.PossibleChar(contours[i])
 		if (possibleChar.intBoundingRectArea > MIN_PIXEL_AREA and
 		possibleChar.intBoundingRectWidth > MIN_PIXEL_WIDTH and possibleChar.intBoundingRectHeight > MIN_PIXEL_HEIGHT and
-		MIN_ASPECT_RATIO < possibleChar.fltAspectRatio and possibleChar.fltAspectRatio < MAX_ASPECT_RATIO):                   # if contour is a possible char, note this does not compare to other chars (yet) . . .
-			intCountOfPossibleChars = intCountOfPossibleChars + 1           # increment count of possible chars
-			listOfPossibleChars.append(possibleChar)                        # and add to list of possible chars
-		# end if
-	    # end for
+		MIN_ASPECT_RATIO < possibleChar.fltAspectRatio and possibleChar.fltAspectRatio < MAX_ASPECT_RATIO):
+			intCountOfPossibleChars = intCountOfPossibleChars + 1
+			listOfPossibleChars.append(possibleChar)
+	listOfListsOfMatchingChars = []
+	for possibleChar in listOfPossibleChars:
+		listOfMatchingChars = findListOfMatchingChars(possibleChar, listOfPossibleChars)
+		listOfMatchingChars.append(possibleChar)
+		if len(listOfMatchingChars) < MIN_NUMBER_OF_MATCHING_CHARS:
 
-	listOfListsOfMatchingChars = []                  # this will be the return value
-	for possibleChar in listOfPossibleChars:                        # for each possible char in the one big list of chars
-		listOfMatchingChars = findListOfMatchingChars(possibleChar, listOfPossibleChars)        # find all chars in the big list that match the current char
-
-		listOfMatchingChars.append(possibleChar)                # also add the current char to current possible list of matching chars
-
-		if len(listOfMatchingChars) < MIN_NUMBER_OF_MATCHING_CHARS:     # if current possible list of matching chars is not long enough to constitute a possible plate
-		    continue                            # jump back to the top of the for loop and try again with next char, note that it's not necessary
-		                                        # to save the list in any way since it did not have enough chars to be a possible plate
-		# end if
-
-		                                        # if we get here, the current list passed test as a "group" or "cluster" of matching chars
-		listOfListsOfMatchingChars.append(listOfMatchingChars)      # so add to our list of lists of matching chars
-
+		    continue
+		listOfListsOfMatchingChars.append(listOfMatchingChars)
 		listOfPossibleCharsWithCurrentMatchesRemoved = []
-
-		                                        # remove the current list of matching chars from the big list so we don't use those same chars twice,
-		                                        # make sure to make a new big list for this since we don't want to change the original big list
 		listOfPossibleCharsWithCurrentMatchesRemoved = list(set(listOfPossibleChars) - set(listOfMatchingChars))
 
-		recursiveListOfListsOfMatchingChars = findListOfListsOfMatchingChars(listOfPossibleCharsWithCurrentMatchesRemoved)      # recursive call
-
-		for recursiveListOfMatchingChars in recursiveListOfListsOfMatchingChars:        # for each list of matching chars found by recursive call
-			listOfListsOfMatchingChars.append(recursiveListOfMatchingChars)             # add to our original list of lists of matching chars
-		# end for
-
-		break       # exit for
-
-	# end for
+		recursiveListOfListsOfMatchingChars = findListOfListsOfMatchingChars(listOfPossibleCharsWithCurrentMatchesRemoved)
+		for recursiveListOfMatchingChars in recursiveListOfListsOfMatchingChars:
+			listOfListsOfMatchingChars.append(recursiveListOfMatchingChars)
+		break
 	listOfListsOfMatchingCharsInScene = listOfListsOfMatchingChars
-	for listOfMatchingChars in listOfListsOfMatchingCharsInScene:                   # for each group of matching chars
-		possiblePlate = extractPlate(imgOriginalScene, listOfMatchingChars)         # attempt to extract plate
+	for listOfMatchingChars in listOfListsOfMatchingCharsInScene:
+		possiblePlate = extractPlate(imgOriginalScene, listOfMatchingChars)
 
-		if possiblePlate.imgPlate is not None:                          # if plate was found
-			listOfPossiblePlates.append(possiblePlate)                  # add to list of possible plates
-		# end if
-	# end for
+		if possiblePlate.imgPlate is not None:
+			listOfPossiblePlates.append(possiblePlate)
 
-	print("\n" + str(len(listOfPossiblePlates)) + " possible plates found")  # 13 with MCLRNF1 image
-	return listOfPossiblePlates[0].imgPlate
+	#Seleção das possíveis placas:
+	#J=0
+	lis = []
+	for l in listOfPossiblePlates:
+		i=0
+		img = cv2.cvtColor(l.imgPlate, cv2.COLOR_BGR2GRAY)
+		img = cv2.resize(img,(512,200))
+		img = abs(255-img)
+		kernel = np.ones((5,5), np.uint8)
+		img1 = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
+		img2 = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
+		img = cv2.add(img,img1)
+		img = cv2.absdiff(img,img2)
+		img=cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,201,0)
+		img = cv2.GaussianBlur(img,(3,3),0)
+		a,img = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+		kernel = np.ones((2,2), np.uint8)
+		img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+		img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+		contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+		for cnt in contours:
+			x,y,w,h = cv2.boundingRect(cnt)
+			if((w>35)&(w<70)&(h>100)&(h<160)&(y>3)&(x>3)):
+				copy=img[y-4:y+h+8, x-4:x+w+8]
+				c = pytesseract.image_to_string(copy,config='--psm 10 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --oem 0')
+				if((c=='A')|(c=='B')|(c=='C')|(c=='D')|(c=='E')|(c=='F')|(c=='G')|(c=='J')|(c=='K')|(c=='L')|(c=='M')|(c=='N')|(c=='O')|(c=='P')|(c=='Q')|(c=='R')|(c=='S')|(c=='T')|(c=='U')|(c=='V')|(c=='W')|(c=='X')|(c=='Y')|(c=='Z')|(c=='0')|(c=='2')|(c=='3')|(c=='4')|(c=='5')|(c=='6')|(c=='7')|(c=='8')|(c=='9')):
+					#print(c)
+					i=i+1
+		if(i<8):
+			#cv2.imshow(('debug'+str(J)),img)
+			#print('added: '+str(i)+','+str(J))
+			lis.append((l.imgPlate,i))
+		#J=J+1
+
+	lis.sort(key = lambda x: int(x[1]),reverse=True)
+	
+
+	#lis[0][0] é a placa escolhida
+	#print(lis[0][1])
+	#cv2.imshow('chosen',lis[0][0])
+	#cv2.waitKey(0)
+	#cv2.destroyAllWindows()
+	return lis[0][0]
+
+
+
